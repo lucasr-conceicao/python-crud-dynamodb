292575a4a1127fe9637d46453f91363c14b67401
lucasr-conceicao <lucasr-conceicao@outlook.com.br>
2025-04-21 20:43:55 -0300

    ajustado teste unitario

--- src/my_script_test.py
+++ src/my_script_test.py
@@ -3,6 +3,6 @@ from src import my_script
 
 def test_main(capsys):
     print("")
-    # my_script.main()
-    # captured = capsys.readouterr()
-    # assert captured.out.strip() == "Hello, world!"
+    my_script.main()
+    captured = capsys.readouterr()
+    assert captured.out.strip() == "Hello, world!"
